9495fc08c0a00f633d971b6869c85b3e22bab6ca
Stephen Melinyshyn <smelinysh@icloud.com>
2015-10-24 10:17:58 -0400

    Inital project template

---
+++ NearbEYE/NearbEYE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NearbEYE
+//
+//  Created by Stephen Melinyshyn on 2015-10-24.
+//  Copyright © 2015 Stephen Melinyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
