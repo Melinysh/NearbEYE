899adba25e0da7db6eeee5403b8fb7194f649905
Aar0nC <acotter@tfs.ca>
2015-10-24 13:31:17 -0400

    Experimental Design

--- NearbEYE/NearbEYE/ViewController.swift
+++ NearbEYE/NearbEYE/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var mealNameLabel: UITextField!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,10 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(sender: UIButton) {mealNameLabel.text = "Default Text"
+    }
 
 }
 
